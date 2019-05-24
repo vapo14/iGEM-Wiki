44ea62c4013d81db35bbbf0be86b794c704c9202
vcitor emmanuel guerra aguado <wasmon999@hotmail.com>
2019-05-24 00:07:46 -0600

    added fade function for card =>name

--- js/team.js
+++ js/team.js
@@ -1,4 +1,13 @@
-$('div.card-header div.card-link').hover(function(){
-    var target = $(this).attr("target");
-    $().toggle();
+$('document').ready(function(){
+    $('li.name').fadeToggle();
+
+
+    var cards = document.getElementsByClassName('cardContainer');
+    $(cards).hover(function()
+    {
+        var t = $(this).attr('target');
+        $(t).fadeToggle();
+    });
+
 });
+
