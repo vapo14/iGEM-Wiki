b229f354d4859c9f901d9e31b159771cf9ad287d
vapo14 <va-po@hotmail.com>
2019-05-25 13:28:20 -0600

    style changes to brainstorming

---
+++ www/js/team.js
@@ -0,0 +1,14 @@
+$('document').ready(function(){
+    $('li.name').hide();
+    //$('ul.names').css('list-style','none');
+
+    var cards = document.getElementsByClassName('cardContainer');
+    $(cards).hover(function()
+    {
+        var t = $(this).attr('target');
+        $(t).stop();
+        $(t).fadeToggle();
+    });
+
+});
+
