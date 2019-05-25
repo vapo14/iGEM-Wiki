b42668abc09457536faeebf739298215b365ab64
vcitor emmanuel guerra aguado <wasmon999@hotmail.com>
2019-05-25 12:57:42 -0600

    Added advisors y sus modals - estilos a definir

--- js/team.js
+++ js/team.js
@@ -1,6 +1,6 @@
 $('document').ready(function(){
-    $('li.name').fadeToggle();
-    $('ul.names').css('list-style','none');
+    $('li.name').hide();
+    //$('ul.names').css('list-style','none');
 
     var cards = document.getElementsByClassName('cardContainer');
     $(cards).hover(function()
