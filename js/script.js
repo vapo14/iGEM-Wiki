2b2d619c553e782206884683706c7e1670592963
vapo14 <va-po@hotmail.com>
2019-06-04 14:51:10 -0600

    project description page outline

--- js/script.js
+++ js/script.js
@@ -94,6 +94,27 @@ $(document).ready(function () {
 
 
 
+//BACK TO TOP BUTTON
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        $("#BackToTopButton").fadeIn() = "block";
+    } else {
+        $("#BackToTopButton").fadeOut() = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 
 
 
