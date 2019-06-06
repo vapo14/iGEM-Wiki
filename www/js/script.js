b6600fb3e3c7c8e06cfc586185c5d839a19d0c58
vapo14 <va-po@hotmail.com>
2019-06-06 15:51:50 -0600

    changes and stuff

--- www/js/script.js
+++ www/js/script.js
@@ -8,10 +8,26 @@ $('ul.nav li.active').hover(function () {
 //     }
 // });
 
+var onScreen = false;
 
-$('#navham').click(function () {
-    $('#VertNav').toggle(500);
-})
+function openMobile(x) {
+    x.classList.toggle("change");
+    if (!onScreen) {
+        $('#VertNav').slideDown(500);
+        onScreen = true;
+    } else {
+        $('#VertNav').slideUp(500);
+        onScreen = false;
+
+    }
+
+}
+
+
+
+// $('#navham').click(function () {
+//     $('#VertNav').toggle(500);
+// })
 
 
 $(function () {
@@ -60,7 +76,6 @@ $(document).ready(function () {
     $('li.name').hide();
     //$('ul.names').css('list-style','none');
 
-
     var cards = document.getElementsByClassName('cardContainer');
     $(cards).hover(function () {
         var t = $(this).attr('target');
@@ -72,7 +87,6 @@ $(document).ready(function () {
         "background-color": color + "0)"
     })
     $(window).scroll(function () {
-
         if ($(window).scrollTop() >= 900) {
             $('.cont').css({
                 "background-color": color + "1)"
@@ -133,7 +147,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         $("#BackToTopButton").fadeIn() = "block";
     } else {
         $("#BackToTopButton").fadeOut() = "none";
